29ad47e99797072d597268b370fa35088b07dfc8
Erik Petrov <koteman123@hotmail.com>
2021-05-09 23:19:33 +0300

    og commit

---
+++ CodingInterviewQuestions/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+
+namespace CodingInterviewQuestions
+{
+    class Program : Questions
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("" +
+                "Coding interview Questions:\n " +
+                "1. How do you reverse a given string in place without using a loop only?\n " +
+                "2. How do you check if a given string is a palindrome?  (e.g. “Taco cat”)\n " +
+                "3. Write a program to display the multiplication table of a given integer.\n " +
+                "4. Write a program  to find the maximum and minimum element in an array.\n " +
+                "5. Write a program to find the largest of three given (input) numbers. (e.g. 8, 6, 10 → 10)\n " +
+                "6. Write a program to calculate the sum of elements in an array. (e.g. 1234 → 1 + 2 + 3 + 4 → 10)\n " +
+                "7. Write a program to output a pyramid-like pattern  with an asterisk.\n " +
+                "8. Write a program to check if a given number is odd or even. Return “odd” if the number is odd, or “even” if the number is even.\n ");
+            string choice = Console.ReadLine();
+            Console.WriteLine();
+            switch (choice)
+            {
+                case "1":
+                    FirstQuestion();
+                    break;
+                case "2":
+                    SecondQuestion();
+                    break;
+                case "3":
+                    ThirdQuestion();
+                    break;
+                case "4":
+                    FourthQuestion();
+                    break;
+                case "5":
+                    FifthQuestion();
+                    break;
+                case "6":
+                    SixthQuestion();
+                    break;
+                case "7":
+                    SeventhQuestion();
+                    break;
+                case "8":
+                    EighthQuestion();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ CodingInterviewQuestions/Questions.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingInterviewQuestions
+{
+    class Questions
+    {
+        static public void FirstQuestion()
+        {
+            Console.WriteLine("Please enter the string you want to reverse: ");
+            char[] text = Console.ReadLine().ToCharArray();
+            Array.Reverse(text);
+            Console.WriteLine(text);
+        }
+        static public void SecondQuestion()
+        {
+            bool abort = true;
+            Console.WriteLine("Please enter a string that you want to check: ");
+            string text = Console.ReadLine();
+            text = String.Concat(text.Where(c => !Char.IsWhiteSpace(c))).ToLower();
+            char[] textAr = new char[text.Length];
+            for (int i = 0; i < text.Length; i++)
+            {
+                textAr[i] = text[i];
+            }
+            char[] chkArray = (char[])textAr.Clone();
+            Array.Reverse(textAr);
+            for (int i = 0; i < textAr.Length; i++)
+            {
+                if (textAr[i] != chkArray[i])
+                {
+                    Console.WriteLine($"{text} isn't a palindrome.");
+                    abort = false;
+                    break;
+                }
+            }
+            if (abort) { Console.WriteLine($"{text} is a palindrome."); }
+        }
+        static public void ThirdQuestion()
+        {
+            Console.WriteLine("The multiplication table of what number do you need?: ");
+            string number = Console.ReadLine();
+            for (int i = 1; i < 11; i++)
+            {
+                Console.WriteLine($"{number} * {i} = {Convert.ToInt32(number) * i}");
+            }
+        }
+        static public void FourthQuestion()
+        {
+            Console.WriteLine("YetToBeDone");
+        }
+        static public void FifthQuestion()
+        {
+            Console.WriteLine("Please enter 3 numbers: ");
+            int first = Convert.ToInt32(Console.ReadLine());
+            int second = Convert.ToInt32(Console.ReadLine());
+            int third = Convert.ToInt32(Console.ReadLine());
+            int[] numbers = { first, second, third };
+            Array.Sort(numbers);
+            Console.WriteLine($"The largest number of these three is: {numbers[2]}");
+        }
+        static public void SixthQuestion()
+        {
+            int total = 0;
+            Console.WriteLine("Please enter 3 numbers: ");
+            int first = Convert.ToInt32(Console.ReadLine());
+            int second = Convert.ToInt32(Console.ReadLine());
+            int third = Convert.ToInt32(Console.ReadLine());
+            int[] numbers = { first, second, third };
+            foreach (var i in numbers)
+            {
+                total += i;
+            }
+            Console.WriteLine($"All three added numbers together total to: {total}");
+        }
+        static public void SeventhQuestion()
+        {
+            string[,] pyramid = new string[3, 5];
+            pyramid[0, 2] = "*";
+            pyramid[1, 1] = "*";
+            pyramid[1, 2] = "*";
+            pyramid[1, 3] = "*";
+            pyramid[2, 0] = "*";
+            pyramid[2, 1] = "*";
+            pyramid[2, 2] = "*";
+            pyramid[2, 3] = "*";
+            pyramid[2, 4] = "*";
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (pyramid[i,j] != "*") { pyramid[i, j] = " "; }
+                    Console.Write(pyramid[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+        static public void EighthQuestion()
+        {
+            Console.WriteLine("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number % 2 == 0) { Console.WriteLine("The number is even."); }
+            else { Console.WriteLine("The number is odd."); }
+        }
+    }
+}
